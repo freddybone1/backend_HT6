c41075c8dd8edfd6da21f06df0bf236e8d054e39
Evhen Hordiienko <hoodback@gmail.com>
2021-01-17 21:29:53 +0200

    small typo fixes

--- home/models.py
+++ home/models.py
@@ -19,8 +19,8 @@ class Student(Model):
     normalized_name = models.CharField(max_length=200, null=True)
     is_active = models.CharField(max_length=200, null=True)
 
-    subject = models.ForeignKey(to='home.Subject', on_delete=models.SET_NULL, null=True, related_name='student', related_query_name='student')
-    book = models.OneToOneField(to='home.Book', on_delete=models.CASCADE, null=True, related_name='student', related_query_name='student')
+    subject = models.ForeignKey(to='home.Subject', on_delete=models.SET_NULL, null=True)
+    book = models.OneToOneField(to='home.Book', on_delete=models.CASCADE, null=True)
 
 
 class Subject(Model):
