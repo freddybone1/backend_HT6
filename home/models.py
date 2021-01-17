99b94dc11c6787b7c9594be1f5e1cb7423e5d2fd
Evhen Hordiienko <hoodback@gmail.com>
2021-01-17 19:32:23 +0200

    add teacher book and subject models to project, set up relaitions between them

--- home/models.py
+++ home/models.py
@@ -6,7 +6,7 @@ class Student(Model):
     """
     Student model with list of attributes
     """
-    id = models.IntegerField(primary_key=True)  # noqa
+    id = models.AutoField(primary_key=True)  # noqa
     name = models.CharField(max_length=200)
     surname = models.CharField(max_length=200)
     age = models.IntegerField(default=0, null=True)
@@ -18,3 +18,21 @@ class Student(Model):
     social_url = models.CharField(max_length=200, null=True)
     normalized_name = models.CharField(max_length=200, null=True)
     is_active = models.CharField(max_length=200, null=True)
+
+    subject = models.ForeignKey(to='home.Subject', on_delete=models.SET_NULL, null=True, related_name='student', related_query_name='student')
+    book = models.OneToOneField(to='home.Book', on_delete=models.CASCADE, null=True, related_name='student', related_query_name='student')
+
+
+class Subject(Model):
+    title = models.CharField(max_length=200)
+
+
+class Book(Model):
+    id = models.AutoField(primary_key=True)  # noqa
+    title = models.CharField(max_length=200)
+
+
+class Teacher(Model):
+    id = models.AutoField(primary_key=True)  # noqa
+    name = models.CharField(max_length=200)
+    students = models.ManyToManyField(to='home.Student', related_name='teachers', related_query_name='teachers')
